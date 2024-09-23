2422dc72cf04a29e5e407f86c8d6a588c30de08f
J0seCamp0s <jgabriel.campos.u@gmail.com>
2024-09-23 12:04:32 -0600

    Update obj files

--- indexer/obj/Debug/net7.0/indexer.AssemblyInfo.cs
+++ indexer/obj/Debug/net7.0/indexer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("indexer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a2db0bfabffa38b06a18d5df378a3fa4bfa20e")]
 [assembly: System.Reflection.AssemblyProductAttribute("indexer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("indexer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
