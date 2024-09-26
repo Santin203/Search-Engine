7a31d72808f10515d0dd2b5cad09b729bfc4623b
Santiago Jimenez <santiago.jimenez.casas@gmail.com>
2024-09-25 20:57:37 -0600

    project stuff

--- indexer/obj/Debug/net7.0/indexer.AssemblyInfo.cs
+++ indexer/obj/Debug/net7.0/indexer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("indexer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72b252722540b162915d6e86a6ef92c9ac0259f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("indexer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("indexer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
